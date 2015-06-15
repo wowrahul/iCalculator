c6d10358a185b15971b237c91dcf08e3501a52a4
wowrahul <rj1022@nyu.edu>
2015-06-15 19:47:15 -0400

    Basic Calculator based on Swift
    A basic calculator based on Swift. This is based on the video tutorial
    from Stanford University.

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -8,18 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+  
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsEditing :Bool = false
+
+    @IBAction func appendDigit(sender: UIButton)
+    {
+        let digit = sender.currentTitle!
+        if userIsEditing {
+            display.text! += digit
+        }
+        else
+        {
+            display.text = digit
+            userIsEditing = true
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
